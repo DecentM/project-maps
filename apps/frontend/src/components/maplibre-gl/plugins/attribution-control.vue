19a308ef767ef568985d169516b2603d17cc5975
DecentM <decentm@decentm.com>
2025-07-23 00:40:37 +0300

    refactor(frontend): remove custom attribution and use builtin

---
+++ apps/frontend/src/components/maplibre-gl/plugins/attribution-control.vue
@@ -0,0 +1,22 @@
+<template>
+  <slot />
+</template>
+
+<script lang="ts" setup>
+import { onBeforeUnmount, onMounted, ref } from 'vue'
+import { AttributionControl, type Map as MaplibreGl } from 'maplibre-gl'
+
+const props = defineProps<{
+  map: MaplibreGl
+}>()
+
+const control = ref(new AttributionControl())
+
+onMounted(() => {
+  props.map.addControl(control.value)
+})
+
+onBeforeUnmount(() => {
+  props.map.removeControl(control.value)
+})
+</script>
--- apps/frontend/src/pages/index-page.vue
+++ apps/frontend/src/pages/index-page.vue
@@ -3,7 +3,6 @@ import { ref } from 'vue'
 
 import LocationSidebar from 'src/components/location-sidebar/location-sidebar.vue'
 import MaplibreGl from 'src/components/maplibre-gl/maplibre-gl.vue'
-import MapAttribution from 'src/components/maplibre-gl/map-attribution.vue'
 
 import PanzoomTrackerPlugin from 'src/components/maplibre-gl/plugins/panzoom-tracker.vue'
 import HoverTrackerPlugin from 'src/components/maplibre-gl/plugins/hover-tracker.vue'
@@ -11,6 +10,7 @@ import GlobeControlPlugin from 'src/components/maplibre-gl/plugins/globe-control
 import GeolocateControlPlugin from 'src/components/maplibre-gl/plugins/geolocate-control.vue'
 import NavigationControlPlugin from 'src/components/maplibre-gl/plugins/navigation-control.vue'
 import ScaleControlPlugin from 'src/components/maplibre-gl/plugins/scale-control.vue'
+import AttributionControlPlugin from 'src/components/maplibre-gl/plugins/attribution-control.vue'
 
 const hoveredPoi = ref<GeoJSON.Feature<GeoJSON.Point, GeoJSON.GeoJsonProperties> | null>(null)
 const selectedPoi = ref<GeoJSON.Feature<GeoJSON.Point, GeoJSON.GeoJsonProperties> | null>(null)
@@ -53,7 +53,7 @@ const handleMapClick = () => {
       :max-pitch="80"
     >
       <template v-slot="{ map }">
-        <map-attribution />
+        <attribution-control-plugin v-if="map" :map="map" />
         <hover-tracker-plugin v-if="map" :map="map" v-model="hoveredPoi" />
         <globe-control-plugin v-if="map" :map="map" />
         <panzoom-tracker-plugin v-if="map" :map="map" />
