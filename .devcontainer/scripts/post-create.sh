f7db0d2ac9d9026391fe2814aafc297df8f7f722
DecentM <decentm@decentm.com>
2025-07-31 22:58:38 +0300

    chore(devcontainer): add vscode extensions

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -1,16 +1,17 @@
 #!/bin/sh -exu
 
+# if the user has set DEVCONTAINER_PACKAGES, install them
+if [ -n "$DEVCONTAINER_PACKAGES" ]; then
+  yay -S --noconfirm $DEVCONTAINER_PACKAGES
+fi
+
 # Install asdf plugins if they are defined in .tool-versions. "asdf plugin add" accepts only one plugin at a time
 if [ -f .tool-versions ]; then
-    while read -r plugin version; do
-        asdf plugin add "$plugin"
-    done < .tool-versions
+  while read -r plugin version; do
+    echo "$plugin $version"
+    asdf plugin add "$plugin"
+  done < .tool-versions
 fi
 
 # Install the versions specified in .tool-versions
 asdf install
-
-# if the user has set DEVCONTAINER_PACKAGES, install them
-if [ -n "$DEVCONTAINER_PACKAGES" ]; then
-    yay -S --noconfirm $DEVCONTAINER_PACKAGES
-fi
