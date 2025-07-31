3dc952e6a9b51f26d7b7c20638b9ac0a03fb5f45
DecentM <decentm@decentm.com>
2025-07-31 20:14:13 +0300

    chore(devcontainer): run pnpm i on start, pull oh-my-posh bash config

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -1,14 +1,16 @@
 #!/bin/sh -exu
 
-if [ command -v asdf >/dev/null 2>&1 ]; then
-    # Install asdf plugins if they are defined in .tool-versions
-    if [ -f /root/.tool-versions ]; then
-        asdf plugin add $(awk '{print $1}' /root/.tool-versions | sort -u)
-    fi
+# Install asdf plugins if they are defined in .tool-versions. "asdf plugin add" accepts only one plugin at a time
+if [ -f .tool-versions ]; then
+    while read -r plugin version; do
+        asdf plugin add "$plugin"
+    done < .tool-versions
+fi
 
-    # Install the versions specified in .tool-versions
-    asdf install
-else
-    echo "asdf is not installed. Please ensure asdf is set up correctly."
-    exit 1
+# Install the versions specified in .tool-versions
+asdf install
+
+# if the user has set DEVCONTAINER_PACKAGES, install them
+if [ -n "$DEVCONTAINER_PACKAGES" ]; then
+    su yay -c 'yay -S --noconfirm $DEVCONTAINER_PACKAGES'
 fi
