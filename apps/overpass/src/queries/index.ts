2b5f06c74b77684d342ad097ae27681728cb7c02
DecentM <decentm@decentm.com>
2025-11-22 20:35:22 +0000

    chore: clean up old overpass service - metadata calls it directly now

--- apps/overpass/src/queries/index.ts
+++
@@ -1,4 +0,0 @@
-export * as ShortRangeNamed from './short-range-named';
-export * as WikidataIdsInRange from './wikidata-ids-in-range';
-export * as PoiMetadata from './poi-metadata';
-export * as PoiWikidataId from './poi-wikidata-id';
